bf557c49ef1dd7c835ed8f624bbd2512f8313709
Shanny <91589562+shanxiaow@users.noreply.github.com>
2024-12-11 17:04:09 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 import streamlit as st
 
 
@@ -52,7 +52,7 @@ with st.sidebar:
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
 
-client = openai(api_key=st.secrets["OPENAI_API_KEY"])
+# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o" #"gpt-3.5-turbo"
